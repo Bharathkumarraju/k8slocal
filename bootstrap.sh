d6348a7bd610222fc0c1d90fcb203e60204588f1
vjsujay <vjsujay@gmail.com>
2019-03-20 10:00:14 +0530

    removed adding nameserver to /etc/resolv.conf

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 ##Script Author: BKR (Cloud & DevOps Enabler :-) ##
-# Included below nameserver line to avoid Could not retrieve mirrorlist error
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
 yum install -y epel-release
 yum install -y vim wget ansible
